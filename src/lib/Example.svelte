fbe810ff875c6c681e8accfd5076469e07b921af
abdel-17 <abdelrahmanb015@gmail.com>
2024-02-21 13:04:06 +0200

    add repoduction

---
+++ src/lib/Example.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	let { foo } = $props<{ foo: Snippet<[string]> }>();
+
+	function bar() {
+		return "baz";
+	}
+</script>
+
+{@render foo(bar())}
